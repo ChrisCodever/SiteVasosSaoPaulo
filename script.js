933cd9a960721ec896f568ca93f999cdeb28bdc9
ChrisCodever <chriscodever@gmail.com>
2024-12-25 20:42:20 -0300

    novas atualizacoes grande mudanca

--- script.js
+++ script.js
@@ -19,3 +19,18 @@ function criarFolha(event) {
 document.querySelectorAll('a').forEach(link => {
   link.addEventListener('mouseover', criarFolha);
 });
+
+
+
+
+function openModal(image) {
+  const modal = document.getElementById("imageModal");
+  const modalImage = document.getElementById("modalImage");
+  modalImage.src = image.src;
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
+}
